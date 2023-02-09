b64e683b2d1e94b990e6889ccac47bf32d9887c5
Pham Khanh Hung <hungpk1412@gmail.com>
2023-02-09 08:59:30 +0700

    added config database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -8,6 +9,16 @@ import { AppService } from './app.service';
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: ['.env'],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'root',
+      password: 'password',
+      database: 'myDB',
+      entities: [],
+      synchronize: true,
     }),
   ],
   controllers: [AppController],
